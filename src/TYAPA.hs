51d2f90cf20d0e2b7b5bff118b5333749ea053b9
Heather Cynede <heather@cynede.net>
2013-04-08 11:38:55 +0400

    cosmetics

--- src/TYAPA.hs
+++ src/TYAPA.hs
@@ -1,5 +1,4 @@
 ﻿{-# LANGUAGE UnicodeSyntax, CPP #-}
-
 import Data.List
 import Data.Function
 import Data.Char
@@ -19,7 +18,7 @@ import Control.Monad
 import Control.Applicative
 
 import Text.Printf
-
+------------------------- Narural Sort algorithm ---------------------------------------
 nSort   ::  [String] → [String]
 nSort s =   if (and . map allFloat) s then    
                 let { readFloat = read :: String → Float } 
@@ -42,47 +41,43 @@ natComp xxs@(x:xs) yys@(y:ys)
             noDigit     =   not . isDigit
             getNumber s =   let { digits = takeWhile isDigit s }
                             in (read digits :: Integer, drop (length digits) s)
-                   
+------------------------------------------------------------------------------------------
 version = "0.1.3"
 main    = do
-
+    -- >
     all ← getDirectoryContents "."
     cd  ← takeBaseName <$> getCurrentDirectory
-
-    let ziped = zip[1..]
-                . nSort
-                . filter (\x → any(`isSuffixOf` map toLower x)
+    -- >
+    let ziped = zip[1..] . nSort
+              . filter (\x → any(`isSuffixOf` map toLower x)
                     [".jpg", ".jpeg", ".png", ".gif", ".bmp"])
                         $ all
-
+    -- >
     skipped ← newIORef 0
     renamed ← newIORef 0
-    
+    -- >
     printf "\n  Tyapa v.%s\n\n" version
     forM_ ziped $ \(i,x) → do
-    
+        -- >
         let fn  = 
                 printf "%s.%d.%d%s" cd (q::Int) (z::Int) sff
                 where sff = map toUpper $ takeExtension x
-                      q   = ceiling (fromIntegral i / 2.0 )
+                      q   = ceiling $ fromIntegral i / 2.0
                       z   = if odd i then 1 else 2
-                      
+        -- >
         printf "  %s --> %s" x fn
         doesFileExist fn >>= \fx → 
             case fx of
                 True -> do
-                    printf "  <- File exist\n"
-                    counter ← readIORef skipped
-                    writeIORef skipped $ counter + 1
+                    printf "  <- File exist\n"  -- skipped + 1
+                    counter ← readIORef skipped; writeIORef skipped $ counter + 1
                 False -> do
-                    printf " <- Renamed\n"
-                    counter ← readIORef renamed
-                    writeIORef renamed $ counter + 1
+                    printf " <- Renamed\n"      -- renamed + 1
+                    counter ← readIORef renamed; writeIORef renamed $ counter + 1
                     renameFile x fn
-                    
+    -- >
     printf "\n"
-    skpd ← readIORef skipped
-    cntr ← readIORef renamed
-    printf "    skipped %d files\n" (skpd::Int)
-    printf "    renamed %d files\n" (cntr::Int)
+    skpd ← readIORef skipped; printf "    skipped %d files\n" (skpd::Int)
+    cntr ← readIORef renamed; printf "    renamed %d files\n" (cntr::Int)
     getChar
+    -- >
